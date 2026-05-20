50359ee7543fa6f1133cbaab9ca952698f0ccbe6
Aysel <aysel@gmail.com>
2026-05-20 11:20:33 +0400

    Zod used

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,103 @@
 import "./App.css";
-import { useForm } from "react-hook-form";
-import type { SubmitHandler } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
-/*interface IForm {
-  name: string;
-  surname: string;
-  email: string;
-  password: string;
-}
-*/
-const schema = yup
+const schema = z
   .object({
-    name: yup.string(),
-    surname: yup.string(),
-    email: yup.string().required(),
-    password: yup.string().required(),
+    name: z.enum(["aysel", "zaxra", "other"]),
+    surname: z
+      .string()
+      .min(1, "surname is required")
+      .max(40, "max 40 characters")
+      .regex(/^[A-Za-z]+$/, "only letters allowed"),
+    email: z.string().min(1, "email is required"),
+    password: z.string().min(1, "password is required"),
+    confirmPassword: z.string().min(1, "password is required"),
   })
-  .required();
-
-function App() {
-  const { register, handleSubmit, watch } = useForm({
-    resolver: yupResolver(schema),
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
   });
 
-  const onSubmit = (data) => console.log(data);
+function App() {
+  const {
+    handleSubmit,
+    control,
+    getValues,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      surname: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    resolver: zodResolver(schema),
+  });
 
-  console.log(watch());
+  const onSubmit = () => console.log(getValues());
 
   return (
     <>
+      <h1>Complete form</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="name">Enter name:</label>
-        {
-          <input
-            defaultValue="Aysel"
-            type="text"
-            {...register("name")}
-            placeholder="enter name . . ."
-          />
-        }
+        {errors.name && (
+          <span className="errorMessage">*{errors.name.message}</span>
+        )}
+        <Controller
+          name="name"
+          control={control}
+          render={({ field }) => <input placeholder="First name" {...field} />}
+        />
+
         <label htmlFor="surname">Enter surname:</label>
-        {
-          <input
-            type="text"
-            defaultValue={"Sadiqova"}
-            placeholder="enter surname"
-            {...register("surname")}
-          />
-        }
+        {errors.surname && (
+          <span className="errorMessage">*{errors.surname.message}</span>
+        )}
+        <Controller
+          name="surname"
+          control={control}
+          render={({ field }) => <input placeholder="Surname" {...field} />}
+        />
+
         <label htmlFor="email">Enter email:</label>
-        {<input type="text" placeholder="enter email" {...register("email")} />}
-        <label htmlFor="name">Enter password:</label>
-        {
-          <input
-            type="password"
-            placeholder="enter password"
-            {...register("password")}
-          />
-        }
-        {<input type="submit" />}
+        {errors.email && (
+          <span className="errorMessage">*{errors.email.message}</span>
+        )}
+        <Controller
+          name="email"
+          control={control}
+          render={({ field }) => <input placeholder="Email" {...field} />}
+        />
+
+        <label htmlFor="password">Enter password:</label>
+        {errors.password && (
+          <span className="errorMessage">*{errors.password.message}</span>
+        )}
+        <Controller
+          name="password"
+          control={control}
+          render={({ field }) => <input placeholder="Password" {...field} />}
+        />
+
+        <label htmlFor="confrimPassword">Enter password:</label>
+        {errors.confirmPassword && (
+          <span className="errorMessage">
+            *{errors.confirmPassword.message}
+          </span>
+        )}
+        <Controller
+          name="confirmPassword"
+          control={control}
+          render={({ field }) => (
+            <input placeholder="confirm Password" {...field} />
+          )}
+        />
+
+        {<input className="submitBtn" type="submit" />}
       </form>
     </>
   );
